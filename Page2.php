156292e27559b2a05af9aa0f2057b53b1204f34b
Theo-LANTZ <71310078+Theo-LANTZ@users.noreply.github.com>
2020-12-14 17:15:53 +0100

    pagestatic

---
+++ Page2.php
@@ -0,0 +1,11 @@
+<html lang="fr">
+<head>
+    <title>Covid Tracking</title>
+</head>
+<body>
+<h1>Page 2</h1>
+<button><a href="index.html">Page Accueil</a></button><br />
+<button><a href="Page1.php">Page1</a></button><br />
+<button><a href="Page3.php">Page3</a></button><br />
+</body>
+</html>
---
+++ Page3.php
@@ -0,0 +1,11 @@
+<html lang="fr">
+<head>
+    <title>Covid Tracking | ETAT COVID</title>
+</head>
+<body>
+<h1>Page 3</h1>
+<button><a href="index.html">Page Accueil</a></button>
+<button><a href="Page1.php">Page1</a></button><br />
+<button><a href="Page2.php">Page2</a></button><br />
+</body>
+</html>
