f343d99440d7bcede9103942ce040596ea33771e
PIGNATONE <leo@pignatone.fr>
2021-01-13 17:53:05 +0100

    NouvelleVersion
    Toujours problème de chemin avec la bdd

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,8 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Controller\bddServeur;
-use bddServeur as GlobalBddServeur;
+use App\Controller\bddServeur\bddServeur;
 
 class HomeController extends AbstractController
 {
@@ -15,8 +14,8 @@ class HomeController extends AbstractController
      */
     public function etat(): Response
     {
-        include('bdd.php');
-        $connexionbdd = new GlobalBddServeur; 
+        include('bddServeur.php');
+        $connexionbdd = new bddServeur; 
         $connexionbdd ->  query($sql); 
         return $this->render('home/etat.html.twig', [
             'controller_name' => 'HomeController',
